d4fa3ae1bd665a45378111d18e600a1b40c6f894
VinnieVenture <vinventurewow@gmail.com>
2022-12-07 13:16:18 +0100

    finished game

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp1
 {
@@ -20,9 +13,92 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        TextBlock latestTextBlockClicked;
+        bool findingMatch = false;
+
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
+
+            SetUpGame();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed/10F).ToString("0.0s");
+            if (matchesFound == 8) { 
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - jeszcze raz?";
+            }
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🦝","🦝",
+                "🐮","🐮",
+                "🐷","🐷",
+                "🐗","🐗",
+                "🐭","🐭",
+                "🐰","🐰",
+                "🐻","🐻",
+                "🐴","🐴"
+            };
+
+            Random random = new Random();
+
+            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                if (textBlock.Name == "timeTextBlock") continue;
+
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                textBlock.Visibility = Visibility.Visible;
+                animalEmoji.RemoveAt(index);
+            }
+            timer.Start();
+            matchesFound = 0;
+            tenthsOfSecondsElapsed = 0;
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (!findingMatch)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                latestTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == latestTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+                matchesFound++;
+            }
+            else
+            { 
+                latestTextBlockClicked.Visibility= Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(matchesFound == 8) 
+            {
+                SetUpGame();
+            }
         }
     }
 }
